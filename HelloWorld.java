ccfa8bef841074c51b40bbb004e6e59c9c6be43a
Рыбакова Полина Александровна <p.rybakova@univ.priv>
2021-09-16 10:04:41 +0300

    Update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello, World!1");
+=======
+        System.out.println("Hello, World!\n");
+>>>>>>> cbb3af6 (Update file HelloWorld.java)
         System.out.println("New Feature!");
     }
 }
